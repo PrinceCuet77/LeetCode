ab6d8ee9e89cdd2ea11304d91dd03b413992cb8e
prince77 <prince.cuet.77@gmail.com>
2020-04-30 17:54:57 +0600

    All are deleted

--- Easy/Array/26 Remove Duplicates from Sorted Array.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        auto ip = unique(nums.begin(), nums.begin()+nums.size()); 
-        nums.resize(distance(nums.begin(), ip)); 
-        
-        return nums.size();
-    }
-};
