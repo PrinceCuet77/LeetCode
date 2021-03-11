8e29e6441890ed6ef8461188da6c1d5e01eed139
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2021-03-11 21:53:58 +0600

    March 11, 2021

---
+++ 590 N-ary Tree Postorder Traversal.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector < int > v; 
+        if ( root == nullptr ) return v; 
+        
+        postorder(root, v); 
+        return v; 
+    }
+    
+    void postorder(Node *root, vector < int >& v) {
+        if ( root == nullptr ) return; 
+        
+        for ( auto x : root->children ) 
+            postorder(x, v); 
+            
+        v.push_back(root->val); 
+    }
+};
