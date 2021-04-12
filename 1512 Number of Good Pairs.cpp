22018f4387a78078a033ca83657b14a479ae153d
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2021-04-13 00:25:06 +0600

    April 11, 2021

--- 1512 Number of Good Pairs.cpp
+++ 1512 Number of Good Pairs.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
         int cnt = 0; 
-        for ( int i = 0; i < nums.size()-1; i++ ) {
-            for ( int j = i+1; j < nums.size(); j++ ) {
-                if ( nums[i] == nums[j] ) cnt++;
-            }
-        }
+        map < int, int > mp; 
+        for ( int i = 0; i < nums.size(); i++ ) {
+            cnt += mp[nums[i]]; 
+            mp[nums[i]]++;
+        }  
         
         return cnt;
     }
